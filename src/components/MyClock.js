de7cfa97a108eb1479afcec8e5a17c0e92c7c214
chetanmanoj <chetanmanoj2001@gmail.com>
2022-11-16 13:20:44 +0530

    Designed structure of app

---
+++ src/components/MyClock.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Clock from 'react-live-clock';
+import './MyClock.css';
+
+
+function MyClock(){
+
+    return (
+      <div className="box">
+      <Clock format={'HH:mm:ss'}
+      ticking={true}
+      className="clock"
+      />
+      </div>
+    )
+}
+export default MyClock;
